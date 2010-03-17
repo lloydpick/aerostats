e1cb848119ff8601743dc5f969ac55046e180028
Lloyd Pick <lloydpick@gmail.com>
2010-03-17 23:00:40 +0000

    missing association

--- app/models/check.rb
+++ app/models/check.rb
@@ -1,6 +1,7 @@
 class Check < ActiveRecord::Base
 
   # Associations
+  belongs_to :service
   has_many :check_logs
   
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -3,5 +3,6 @@ class Service < ActiveRecord::Base
   # Associations
   has_many :asset_services
   has_many :assets, :through => :asset_services
+  has_many :checks
 
 end
