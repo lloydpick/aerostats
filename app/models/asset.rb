81ba148f03ecc4bd3909660ade1c3943e5409303
Lloyd Pick <lloydpick@gmail.com>
2010-03-17 23:31:38 +0000

    added some validations to asset

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,7 +1,12 @@
 class Asset < ActiveRecord::Base
 
   # Associations
-  has_many :asset_services
+  has_many :asset_services, :dependent => :destroy
   has_many :services, :through => :asset_services
+
+  # Validations
+  validates :name, :presence => true
+  validates :ip_address, :presence => true
+  validates :host_name, :presence => true
 
 end
